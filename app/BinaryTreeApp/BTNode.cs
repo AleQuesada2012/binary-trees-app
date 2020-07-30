546e056ff55637550e538f0d689571b8feb15cf9
JoeA42 <joe.alvarez42@gmail.com>
2020-07-29 20:32:17 -0600

    [added methods to data structures] Added Basic methods to data structures

--- app/BinaryTreeApp/BTNode.cs
+++ app/BinaryTreeApp/BTNode.cs
@@ -1,7 +1,13 @@
-﻿namespace BinaryTreeApp {
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
+
+namespace BinaryTreeApp {
     public class BTNode
     {
         private int data { get; set; }
+        
         private BTNode left { get; set; }
         private BTNode right { get; set; }
 
@@ -9,6 +15,42 @@
         {
             left = null;
             right = null;
+        }
+
+        private BTNode(BTNode left, BTNode right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public int GetData()
+        {
+            return data;
+        }
+
+        public void SetData(int data)
+        {
+            this.data = data;
+        }
+
+        public BTNode GetLeft()
+        {
+            return left;
+        }
+
+        public void SetLeft(BTNode left)
+        {
+            this.left = left;
+        }
+
+        public BTNode GetRight()
+        {
+            return right;
+        }
+
+        public void SetRight(BTNode right)
+        {
+            this.right = right;
         }
     }
 
--- app/BinaryTreeApp/BinaryTree.cs
+++ app/BinaryTreeApp/BinaryTree.cs
@@ -1,4 +1,8 @@
-﻿namespace BinaryTreeApp
+﻿using System;
+using System.Collections.Concurrent;
+using Android.Sax;
+
+namespace BinaryTreeApp
 {
     public class BinaryTree
     {
@@ -6,6 +10,102 @@
 
         public BinaryTree() {
             root = null;
+        }
+
+        public bool contains(int data)
+        {
+            return this.contains(data, this.root);
+        }
+
+        private bool contains(int data, BTNode node)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+            else
+            {
+                if (data < node.GetData())
+                {
+                    return contains(data, node.GetLeft());
+                }
+                else if(data > node.GetData())
+                {
+                    return contains(data, node.GetRight());
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public int getData(int data)
+        {
+            return getData(data, this.root);
+        }
+
+        private int getData(int data, BTNode node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            if (data < node.GetData())
+            {
+                return getData(data, node.GetLeft());
+            }
+            else if (data > node.GetData())
+            {
+                return getData(data, node.GetRight());
+            }
+            else
+            {
+                return node.GetData();
+            }
+        }
+
+        public void Insert(int data)
+        {
+            this.root = this.Insert(data, this.root);
+        }
+
+        private BTNode Insert(int data, BTNode tmp)
+        {
+            if (tmp == null)
+            {
+                BTNode node = new BTNode();
+                node.SetData(data);
+                return node;
+            }
+
+            if (data < tmp.GetData())
+            {
+                tmp.SetLeft(this.Insert(data, tmp.GetLeft()));
+            }
+            else if(data>tmp.GetData())
+            {
+                tmp.SetRight(this.Insert(data, tmp.GetRight()));
+            }
+
+            return tmp;
+        }
+
+        public String PreOrder()
+        {
+            return PreOrder(this.root);
+        }
+        private String PreOrder(BTNode node)
+        {
+            String value = "";
+            if (node != null)
+            {
+                value =  value + node.GetData();
+                value = value + PreOrder(node.GetLeft());
+                value = value + PreOrder(node.GetRight());
+            }
+            return value;
         }
     }
 }
