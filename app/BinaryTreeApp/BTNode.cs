75b13281daf614660c2d7427649fea845f063c6e
AleQuesada2012 <AleQuesada2012@users.noreply.github.com>
2020-07-29 19:05:11 -0600

    tree-node-class

---
+++ app/BinaryTreeApp/BTNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace BinaryTreeApp {
+    public class BTNode
+    {
+        private int data { get; set; }
+        private BTNode left { get; set; }
+        private BTNode right { get; set; }
+
+        public BTNode()
+        {
+            left = null;
+            right = null;
+        }
+    }
+
+
+}
